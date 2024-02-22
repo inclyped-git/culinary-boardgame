884d4009fb3dd97c18c4e67b051a4f153543f26c
inclyped <shlolitsme@gmail.com>
2024-02-22 22:12:18 +0700

    Added ESSENTIALS
    Added the folder required by the source code to extract restaurant details.

---
+++ ESSENTIALS/restaurant_medium.py
@@ -0,0 +1,27 @@
+board_size = 5
+
+delimiter = "/"
+
+csv_data = ["Board Position/Type/Restaurant Name/Price",
+"0/Desserts & Beverages/SunKing Donuts/65",
+"1/Fast Food/Pizza Whut/60",
+"2/Farm-to-Table/Map of the Fowl/125",
+"3/Desserts & Beverages/Whisk Takers/50",
+"4/Farm-to-Table/Nacho Average Joint/90",
+"5/Farm-to-Table/Wurst Gastronomy/125",
+"6/Desserts & Beverages/Starlucks/65",
+"7/Desserts & Beverages/Holy Crepe!/50",
+"8/Fusion Cuisine/AbraKEBABra/100",
+"9/Fast Food/4Toes Chicken/60",
+"10/Farm-to-Table/Frying Nemo/90",
+"11/Desserts & Beverages/Cereal Killers/50",
+"12/Farm-to-Table/Poultry in Motion/90",
+"13/Fast Food/MarryCrown/75",
+"14/Fusion Cuisine/SUShi HQ/100",
+"15/Fusion Cuisine/Wok This Way/80",
+"16/Fusion Cuisine/NaanTheLess/80",
+"17/Fusion Cuisine/Pho-nomenal Delights/80",
+"18/Fast Food/Burger Prince/60",
+"19/Fast Food/Jollysquee/75"]
+
+
---
+++ ESSENTIALS/restaurant_small.py
@@ -0,0 +1,19 @@
+board_size = 4
+
+delimiter = ", "
+
+csv_data = ["Restaurant Name, Type, Price, Board Position",
+"The Mein Event, Fusion Cuisine, 80, 0",
+"Donut Worry Be Happy, Desserts & Beverages, 50, 1",
+"Mama Jones, Fast Food, 60, 2",
+"Poultry in Motion, Fusion Cuisine, 80, 3",
+"Lord of the Fries, Fast Food, 60, 4",
+"The Codfather, Farm-to-Table, 100, 5",
+"Fowl Play, Farm-to-Table, 90, 6",
+"I Knead Pastries, Desserts & Beverages, 65, 7",
+"Wok This Way, Fusion Cuisine, 80, 8",
+"Kelantan Fried Chicken, Fast Food, 60, 9",
+"Vincent Van Dough, Desserts & Beverages, 50, 10",
+"Lamb-or-Linguine, Farm-to-Table, 90, 11"]
+
+
